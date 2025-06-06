a282532eba3abbabccb6cf1107c82cba4d211268
Nishita Singh <141266119+nishitasinghhh@users.noreply.github.com>
2025-06-06 23:39:01 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -51,7 +51,7 @@ app.post('/login', (req,res)=>{
                        res.cookie("token", token, {
   httpOnly: true,
   secure: true,
-  sameSite: "None",
+  sameSite: "none",
   maxAge: 24 * 60 * 60 * 1000
 });
 
