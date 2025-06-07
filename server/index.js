0428d2706b11a255ac698c16f4600bebda8fe1bb
Nishita Singh <141266119+nishitasinghhh@users.noreply.github.com>
2025-06-07 11:48:38 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -229,4 +229,7 @@ mongoose.connect(`${process.env.MONGO_URL}`)
 
 
 
-module.exports = app;
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
